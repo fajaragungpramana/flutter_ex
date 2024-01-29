16e6991038c07f7c2f3808599659c43f96adff73
fajaragungpramana <fajar.agungpramana>
2024-01-29 23:05:54 +0700

    Improvement register.

--- lib/pages/register/register_page.dart
+++ lib/pages/register/register_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ex/pages/register/register_controller.dart';
+import 'package:flutter_ex/widgets/views/ex_button.dart';
 import 'package:flutter_ex/widgets/views/ex_flexible_space_bar.dart';
+import 'package:flutter_ex/widgets/views/ex_text_field.dart';
+import 'package:flutter_ex/widgets/views/ex_text_field_secure.dart';
 import 'package:get/get.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -55,122 +58,54 @@ class RegisterPage extends GetView<RegisterController> {
 
                               const SizedBox(height: 40),
 
-                              Obx(() => TextField(
-                                  decoration: InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: const BorderSide(color: AppColor.gray50),
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      labelText: AppLocalizations.of(context)!.fullName,
-                                      errorText: _registerController.fullNameErrorMessage.isEmpty ? null : _registerController.fullNameErrorMessage
-                                  ),
-                                  onChanged: (text) {
-                                    _registerController.setFullName(text);
-                                  })),
+                              Obx(() => ExTextField(
+                                labelText: AppLocalizations.of(context)!.fullName,
+                                errorText: _registerController.fullNameErrorMessage,
+                                onChanged: (text) {
+                                  _registerController.setFullName(text);
+                                }
+                              )),
 
-                              const SizedBox(height: 8),
+                              const SizedBox(height: 16),
 
-                              Obx(() => TextField(
-                                  decoration: InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: const BorderSide(color: AppColor.gray50),
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      labelText: AppLocalizations.of(context)!.email,
-                                      errorText: _registerController.emailErrorMessage.isEmpty ? null : _registerController.emailErrorMessage
-                                  ),
-                                  onChanged: (text) {
+                              Obx(() => ExTextField(
+                                labelText: AppLocalizations.of(context)!.email,
+                                errorText: _registerController.emailErrorMessage,
+                                onChanged: (text) {
                                     _registerController.setEmail(text);
-                                  })),
+                                }
+                              )),
 
-                              const SizedBox(height: 8),
+                              const SizedBox(height: 16),
 
-                              Obx(() => TextField(
-                                  decoration: InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: const BorderSide(color: AppColor.gray50),
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      labelText: AppLocalizations.of(context)!.password,
-                                      suffixIcon: IconButton(
-                                          icon: Icon(
-                                              _registerController.isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                                              color: AppColor.gray50
-                                          ),
-                                          onPressed: () {
-                                            _registerController.setPasswordVisibility();
-                                          }),
-                                      errorText: _registerController.passwordErrorMessage.isEmpty ? null : _registerController.passwordErrorMessage
-                                  ),
-                                  obscureText: !_registerController.isPasswordVisible,
-                                  enableSuggestions: false,
-                                  autocorrect: false,
-                                  onChanged: (text) {
+                              Obx(() => ExTextFieldSecure(
+                                labelText: AppLocalizations.of(context)!.password,
+                                errorText: _registerController.passwordErrorMessage,
+                                onChanged: (text) {
                                     _registerController.setPassword(text);
-                                  })),
+                                }
+                              )),
 
-                              const SizedBox(height: 8),
+                              const SizedBox(height: 16),
 
-                              Obx(() => TextField(
-                                  decoration: InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: const BorderSide(color: AppColor.gray50),
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10)
-                                      ),
-                                      labelText: AppLocalizations.of(context)!.confirmPassword,
-                                      suffixIcon: IconButton(
-                                          icon: Icon(
-                                              _registerController.isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                                              color: AppColor.gray50
-                                          ),
-                                          onPressed: () {
-                                            _registerController.setConfirmPasswordVisibility();
-                                          }),
-                                      errorText: _registerController.confirmPasswordErrorMessage.isEmpty ? null : _registerController.confirmPasswordErrorMessage
-                                  ),
-                                  obscureText: !_registerController.isConfirmPasswordVisible,
-                                  enableSuggestions: false,
-                                  autocorrect: false,
+                              Obx(() => ExTextFieldSecure(
+                                  labelText: AppLocalizations.of(context)!.confirmPassword,
+                                  errorText: _registerController.confirmPasswordErrorMessage,
                                   onChanged: (text) {
                                     _registerController.setConfirmPassword(text);
-                                  })),
+                                  }
+                              ))
 
                             ],
                           ),
                         )
                     ),
 
-                    SizedBox(
-                        width: double.infinity,
-                        child: Obx(() => ElevatedButton(
-                            onPressed: _registerController.isRegisterEnable ? () {} : null,
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColor.green100,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12)
-                                )
-                            ),
-                            child: Text(
-                                AppLocalizations.of(context)!.signIn,
-                                style: AppStyle.textSemiBold(
-                                    fontSize: 16,
-                                    color: Colors.white
-                                )
-                            )
-                        ))
-                    ),
+                    Obx(() => ExButton(
+                        labelText: AppLocalizations.of(context)!.signIn,
+                        enable: _registerController.isRegisterEnable,
+                        onPressed: () { }
+                    )),
 
                     Row(
                         mainAxisAlignment: MainAxisAlignment.center,
