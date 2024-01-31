5db42db51be59b0c63881467c0ced2736b91cb62
fajaragungpramana01 <fajar.agungpramana@mareco.id>
2024-01-31 22:21:40 +0700

    Create app service and provide post request with json body.

--- lib/core/remote/app/app_remote_service.dart
+++ lib/core/remote/app/app_remote_service.dart
@@ -1,10 +1,12 @@
+import 'package:flutter_ex/core/app/app_response.dart';
+import 'package:flutter_ex/core/remote/app/app_remote_url.dart';
 import 'package:get/get_connect/connect.dart';
 
 class AppRemoteService extends GetConnect {
 
   @override
   void onInit() {
-    httpClient.baseUrl = "http://192.168.0.141:8080/api/";
+    httpClient.baseUrl = AppRemoteUrl.baseUrl;
     httpClient.timeout = const Duration(seconds: 10);
 
     httpClient.addAuthenticator<Object?>((request) {
@@ -13,6 +15,26 @@ class AppRemoteService extends GetConnect {
     });
 
     super.onInit();
+  }
+
+  Future<AppResponse<dynamic>> postRequest(String url, dynamic body) async {
+    try {
+      var response = await post(
+          url,
+          body,
+          contentType: 'application/json'
+      );
+
+      if (response.isOk) {
+        var data = response.body['data'];
+        return AppResponse.success(data: data);
+      } else {
+        var message = response.body['message'];
+        return AppResponse.failure(message: message);
+      }
+    } catch (e) {
+      return AppResponse.error(exception: e);
+    }
   }
 
 }
