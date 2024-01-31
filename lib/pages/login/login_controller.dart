f004084bb50a7fc1d516c938279b17882077c025
fajaragungpramana01 <fajar.agungpramana@mareco.id>
2024-01-31 22:23:58 +0700

    Inject dependencies and provide login function.

--- lib/pages/login/login_controller.dart
+++ lib/pages/login/login_controller.dart
@@ -1,9 +1,13 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ex/core/domain/auth/auth_use_case.dart';
+import 'package:flutter_ex/core/remote/auth/request/login_request.dart';
 import 'package:get/get.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginController extends GetxController {
+
+  final _authUseCase = Get.find<AuthUseCase>();
 
   final RxBool _isLoginEnable = false.obs;
   bool get isLoginEnable => _isLoginEnable.value;
@@ -37,6 +41,17 @@ class LoginController extends GetxController {
   void _onLoginEnable() async {
     _isLoginEnable.value = _email.isNotEmpty && _emailErrorMessage.isEmpty &&
         _password.isNotEmpty && _passwordErrorMessage.isEmpty;
+  }
+
+  Future<void> onLogin() async {
+    var result = await _authUseCase.login(LoginRequest(email: _email.value, password: _password.value));
+    result.when(
+        success: (data) {
+        },
+        failure: (message) {
+        },
+        error: (e) {
+        });
   }
 
 }
