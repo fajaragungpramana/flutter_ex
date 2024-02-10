8a704e94a4d0377659e0a2b412db4917cc37c9b5
fajaragungpramana <fajar.agungpramana>
2024-02-11 01:53:21 +0700

    Create bottom sheet for list category.

---
+++ lib/widgets/bottom_sheets/category_bottom_sheet.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ex/core/data/remote/category/response/category_response.dart';
+import 'package:flutter_ex/extension/string_extension.dart';
+import 'package:flutter_ex/resources/values/app_style.dart';
+import 'package:flutter_ex/widgets/views/ex_bottom_sheet.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class CategoryBottomSheet extends StatelessWidget {
+  final List<CategoryResponse> listCategory;
+  final Function(CategoryResponse) onItemTap;
+
+  const CategoryBottomSheet({
+    Key? key,
+    required this.listCategory,
+    required this.onItemTap
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ExBottomSheet(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Text(
+              AppLocalizations.of(context)!.chooseCategory,
+              style: AppStyle.textSemiBold(),
+            ),
+
+            const SizedBox(height: 8),
+
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: listCategory.length,
+                itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: GestureDetector(
+                        onTap: () => { onItemTap(listCategory[index]) },
+                        child: Text(
+                          CategoryResponse.mapName(context, listCategory[index].name.orEmpty),
+                          style: AppStyle.textRegular(),
+                          textAlign: TextAlign.start,
+                        )
+                    )
+                )
+            )
+
+          ]
+      )
+  );
+
+}
