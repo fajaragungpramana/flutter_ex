cbbc1249485db3ce3cb753fe13d8d058bbe5c512
fajaragungpramana <fajar.agungpramana>
2024-02-10 23:26:48 +0700

    Create transaction item list widget.

---
+++ lib/widgets/items/transaction_item.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_ex/core/data/remote/transaction/response/transaction_response.dart';
+import 'package:flutter_ex/extension/double_extension.dart';
+import 'package:flutter_ex/extension/string_extension.dart';
+import 'package:flutter_ex/gen/assets.gen.dart';
+import 'package:flutter_ex/resources/values/app_color.dart';
+import 'package:flutter_ex/resources/values/app_style.dart';
+
+class TransactionItem extends StatelessWidget {
+  final TransactionResponse transactionResponse;
+  final Function(TransactionResponse) onTapItem;
+
+  const TransactionItem({
+    Key? key,
+    required this.transactionResponse,
+    required this.onTapItem
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    onTap: () => { onTapItem(transactionResponse) },
+    child: Container(
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        child: Row(
+            children: [
+
+              transactionResponse.category == "TOP_UP" ?
+              Assets.lib.resources.drawables.icTransactionDebit.image() : Assets.lib.resources.drawables.icTransactionCredit.image(),
+
+              const SizedBox(width: 16),
+
+              Expanded(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        Text(
+                            transactionResponse.name.orEmpty,
+                            style: AppStyle.textSemiBold(fontSize: 14),
+                            overflow: TextOverflow.ellipsis
+                        ),
+
+                        const SizedBox(height: 4),
+
+                        Text(
+                            transactionResponse.description.orEmpty,
+                            style: AppStyle.textRegular(color: AppColor.black50),
+                            overflow: TextOverflow.ellipsis
+                        )
+
+                      ]
+                  )
+              ),
+
+              const SizedBox(width: 16),
+
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+
+                    Text(
+                        transactionResponse.payment!.amount.orZero.currencyFormat,
+                        style: AppStyle.textSemiBold(fontSize: 14)
+                    ),
+
+                    const SizedBox(height: 4),
+
+                    Text(
+                      transactionResponse.createdAt.dateFormat,
+                      style: AppStyle.textRegular(fontSize: 12, color: AppColor.black50),
+                    )
+
+                  ]
+              )
+
+            ]
+        )
+    )
+  );
+
+}
